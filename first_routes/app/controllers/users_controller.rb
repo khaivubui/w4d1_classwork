736187319dffd14444f3fccc4135694cfda6142d
Khai Bui <khaivubui@gmail.com>
2017-08-21 15:06:04 -0700

    Finish first proj

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm users#index"
+    render json: User.all
+  end
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save
+      render json: new_user
+    else
+      render json: new_user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+  end
+
+  def user_params
+    params[:user].permit(:name, :email)
   end
 end
--- first_routes/db/seeds.rb
+++ first_routes/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jon = User.create(name: 'jon', email: 'jon@snow.com')
+rick = User.create(name: 'rick', email: 'rick@sanchez.com')
